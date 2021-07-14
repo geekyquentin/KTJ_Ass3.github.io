b8f6ed42b8f83f1248c15334727cc40be40452ce
Yashwant Krishna Pagoti <pagotiyashwantkrishna@gmail.com>
2021-07-14 11:22:28 +0530

    fixed warning in Alert.js

--- src/pages/Dashboard/Alert.js
+++ src/pages/Dashboard/Alert.js
@@ -1,20 +1,20 @@
-import React,{useEffect} from 'react'
+import React, { useEffect } from 'react'
 import './App.css'
-function Alert({msg,type,removeAlert,list}) {
-  
+function Alert({ msg, type, removeAlert, list }) {
+
     useEffect(() => {
-       const timeout=setTimeout(() => {
-           removeAlert();
-       }, 2000);
-       return ()=> clearTimeout(timeout);
-    }, [list])
+        const timeout = setTimeout(() => {
+            removeAlert();
+        }, 2000);
+        return () => clearTimeout(timeout);
+    }, [list, removeAlert])
 
 
 
     return <p className={`alert alert-${type}`}>
-          {msg} 
-        </p>
-    
+        {msg}
+    </p>
+
 }
 
 export default Alert
